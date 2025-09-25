e3bf91b7b7b7bb42de0ebc1ed8ca663dadef3b79
Ravshan047 <ravshanraxmatuayev34@gmail.com>
2025-09-25 17:10:23 +0500

    Fixed line endings for settings.py

--- vobkentnew/settings.py
+++ vobkentnew/settings.py
@@ -81,19 +81,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # CORS middleware birinchi bo'lishi kerak
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
 ]
-
 ROOT_URLCONF = 'vobkentnew.urls'
 
 TEMPLATES = [
